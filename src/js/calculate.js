4157e0e320901455327c873c13391d0891d0d638
Fabrício Taveira <fabriciotaveira176@gmail.com>
2023-11-27 15:51:56 -0300

    second commit

--- src/js/calculate.js
+++ src/js/calculate.js
@@ -1,13 +1,28 @@
 
-function calculate(){
+function calculate() {
     const number = document.getElementById("number").value;
+    const output = document.getElementById("output");
     let sum = 0;
+    let resultingNumbers = [];
 
-    for (let i = 1; i < number; i++){
-        if(i%3 === 0 || i%5 === 0){
-            sum += i;
+    if(isNaN(number)){
+        output.innerText = `Texto inválido, por favor digite um número`;
+        return alert(`Texto inválido, por favor digite um número`);
+    }else if(number < 0) {
+        output.innerText = `Por favor, digite um número positivo`;
+        return alert(`Por favor, digite um número positivo`)
+    } else {
+        for (let i = 1; i < parseInt(number); i++) {
+            if (i % 3 === 0 || i % 5 === 0) {
+                sum += i;
+                resultingNumbers.push(i);
+            }
         }
     }
 
-    return window.alert(`O Somatório dos números divisíveis por 3 e 5 de ${number} é : ${sum}`);
+    output.innerText = `O Somatório dos números divisíveis por 3 e 5 de ${number} é : ${sum}
+                       Números resultantes: ${resultingNumbers}`;
+    return alert(`O Somatório dos números divisíveis por 3 e 5 de ${number} é : ${sum} \nNúmeros resultantes: ${resultingNumbers}`);
 }
+
+
